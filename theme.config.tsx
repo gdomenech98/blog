de283115792d6c8c4a68d5fe17d50c7d81389b33
Gerard <gerard@protofy.xyz>
2026-01-19 01:03:45 +0100

    fix light theme and fix edd to english

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,6 +3,11 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Gerard Domenech</span>,
+  nextThemes: {
+    defaultTheme: 'light',
+    forcedTheme: 'light'
+  },
+  darkMode: false,
   project: {
     link: 'https://github.com/gdomenech98/blog',
   },
